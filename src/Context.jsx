788a3441c9fc532085c5379fc40b2214ad6a214e
asblazquez <asanchezb23@gmail.com>
2023-07-14 18:55:00 +0200

    Cambios en estilos + Pager

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,7 +3,7 @@ import { useState, createContext } from 'react'
 const GlobalContext = createContext()
 
 const GlobalProvider = ({ children }) => {
-  const [numPokemons, setNumPokemons] = useState('24')
+  const [numPokemons, setNumPokemons] = useState(30)
 
   const updateNumPokemons = (num) => {
     setNumPokemons(num)
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,33 +3,45 @@ import { apiGetAllPokemon } from '../Api'
 import { PokemonCard } from '../Components/PokemonCard'
 import { GlobalContext } from '../Context'
 import { Loader } from '../Components/Loader'
-import { Pager } from '../Components/Pager'
+import ReactPaginate from 'react-paginate'
 
 export function Home () {
-  const [allPokemons, setAllPokemon] = useState([])
-  const [isLoad, setIsLoad] = useState(false)
   const { numPokemons, updateNumPokemons } = useContext(GlobalContext)
 
-  const handleRangeChange = (event) => {
-    const newValue = event.target.value
-    updateNumPokemons(newValue)
-  }
+  const [allPokemons, setAllPokemon] = useState([])
+  const [isLoad, setIsLoad] = useState(false)
+  const [pageNumber, setPageNumber] = useState(0)
+  const pagesVisited = pageNumber * numPokemons
+
+  // const handleRangeChange = (event) => {
+  //   const newValue = event.target.value
+  //   updateNumPokemons(newValue)
+  // }
 
   useEffect(() => {
     const getPokemons = () => {
       setIsLoad(false)
-      apiGetAllPokemon(setAllPokemon, numPokemons, setIsLoad)
+      apiGetAllPokemon(setAllPokemon, '1000', setIsLoad)
     }
 
     getPokemons()
   }, [numPokemons])
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected)
+  }
+
+  const displayPokemons = (
+    allPokemons
+      .slice(pagesVisited, pagesVisited + numPokemons)
+      .map((pokemon) => (
+        <PokemonCard pokemon={pokemon} key={pokemon.id}/>
+      ))
+  )
+
   return (
-    <div className="container mt-3">
+    <div className="container-fluid mt-3">
       {isLoad ? null : <Loader />}
-      <div>
-        <input className='mr-2 nes-range' type='range' min={1} max={100} value={numPokemons} onChange={handleRangeChange}/>
-        <input type="text" id="dark_field" className="nes-input is-bordered w-1" value={numPokemons} />
-      </div>
       <div className="row justify-center">
         <div className="col-md-6 col-12">
           <div className="nes-field mr-2">
@@ -38,20 +50,31 @@ export function Home () {
           </div>
         </div>
         <div className="col-md-2 col-12 align-self-end">
-          <button type="button" className="nes-btn is-primary">
+          <button type="button" className="nes-btn is-primary w-full">
             Buscar
           </button>
         </div>
       </div>
       <div className='pokemonList mt-3'>
-          {Array.isArray(allPokemons) && allPokemons.map(pokemon => {
-            return (
-              <PokemonCard pokemon={pokemon} key={pokemon.id}/>
-            )
-          })}
+          {displayPokemons}
       </div>
-      {<Pager lPOkemons={allPokemons}/>}
+      <div className='mt-3'>
+        <ReactPaginate
+          previousLabel={'Anterior'}
+          nextLabel={'Siguiente'}
+          pageCount={Math.ceil(allPokemons.length / numPokemons)}
+          onPageChange={changePage}
+          containerClassName={'row justify-center'}
+          activeClassName={'is-disabled'}
+          pageClassName={'nes-button'}
+          previousClassName={'nes-button is-primary'}
+          nextClassName={'nes-button is-primary'}
+          previousLinkClassName={'nes-pointer'}
+          nextLinkClassName={'nes-pointer'}
+          disabledClassName={'is-disabled'}
+          breakClassName={'nes-brake'}
+        />
+      </div>
     </div>
-
   )
 }
